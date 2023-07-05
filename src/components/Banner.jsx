89c94521a77f8cb5d636c2c8ae31d9d2fc7639a2
Shubham Kumar Mandal <shubhampaul756@gmail.com>
2023-07-06 00:54:32 +0530

    first commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,55 @@
+import Carousel from 'react-multi-carousel';
+import { Box, styled } from '@mui/material';
+import 'react-multi-carousel/lib/styles.css';
+
+
+const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+};
+
+const StyledBanner = styled('img')({
+    width: '100%'
+})
+
+
+
+const Banner = ({movies}) =>{
+    return(
+        <Box style={{width: '65%'}}>
+            <Carousel
+                responsive={responsive}
+                swipeable={false}
+                draggable={false}
+                infinite={true}
+                autoPlay={true}
+                autoPlaySpeed={3000}
+                keyBoardControl={true}
+                showDots={false}
+                slidesToSlide={1}
+                containerClass="react-multi-carousel-list"
+                dotListClass="custom-dot-list-style"
+                itemClass="carousel-item-padding-40-px"
+                style={{ overflow: 'visible' }} 
+            >
+                 {
+                    movies.map(movie =>(
+                       <StyledBanner src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="banner"/>
+                    ))
+                 }
+            </Carousel>
+        </Box>
+    )
+} 
+
+export default Banner;
